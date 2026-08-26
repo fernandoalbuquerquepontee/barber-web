d1a5a538dcd8816ba42f792d3d23ecacc9adc14e
Fernando Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-08-26 20:54:06 -0300

    chore: add shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div className="text-4xl font-bold text-red-600">Hello world</div>;
+  return (
+    <div className="text-4xl font-bold text-red-600">
+      Hello world
+      <Button>Fernando</Button>
+    </div>
+  );
 }
 
 export default App;
