a299c06089172c1417034ae87629003685576209
Fernando Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-08-28 16:52:57 -0300

    chore: add react query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import "./index.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { HomePage } from "./pages/home";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HomePage />
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <HomePage />
+    </QueryClientProvider>
+  </StrictMode>,
 );
