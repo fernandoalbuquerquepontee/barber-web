e3e976a2cf6c6d24447529fa7298a643be4484bf
Fernando Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-09-18 09:44:57 -0300

    chore: update API base URL to use environment variable and add .env to .gitignore

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,10 +2,8 @@ import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:8080",
-  plugins: [
-    adminClient() // Adicione o plugin no client
-  ]
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  plugins: [adminClient()],
 });
 
 export const { signIn, signOut, useSession } = authClient;
